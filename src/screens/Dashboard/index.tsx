dfdf546a4b47b10cd463ec480340406f28d45637
fabricioselotto <fabricioselottohard01@gmail.com>
2021-10-29 14:13:19 -0300

    HighlightsCards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Container, Title } from './styles';
+import { HighlightCard } from '../../components/HighlightCard';
+
+import { Container, Header, UserWrapper, UserInfo, Photo, User, UserGreeting, UserName, Icon, HighlightCards } from './styles';
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/37161663?v=4' }}
+                        />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Fabrício</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
+
+            <HighlightCards>
+                <HighlightCard type='up' title='Entradas' amount='R$ 17,400,00' lastTransaction='Última entrada dia 13 de Abril' />
+                <HighlightCard type='down' title='Saídas' amount='R$ 1.259,00' lastTransaction='Última saída dia 03 de Abril' />
+                <HighlightCard type='total' title='Total' amount='R$ 16.141.00' lastTransaction='01 à 16 de Abril' />
+            </HighlightCards>
+
         </Container>
     );
 }
